6334db6e45d38fa76ff4ed54f3f3b37ab6dc0700
Mayra Coelho <mayraccoelho@gmail.com>
2022-05-13 23:00:27 -0300

    runner e steps

---
+++ src/test/java/br/com/mcoelho/steps/LoginStep.java
@@ -0,0 +1,65 @@
+package br.com.mcoelho.steps;
+
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
+
+public class LoginStep {
+    @Dado("que a modal esteja sendo exibida")
+    public void queAModalEstejaSendoExibida() {
+
+    }
+
+    @Quando("for realizado um clique fora da modal")
+    public void forRealizadoUmCliqueForaDaModal() {
+
+    }
+
+    @Entao("a janela modal deve ser fechada")
+    public void aJanelaModalDeveSerFechada() {
+
+    }
+
+    @Quando("for realizado um clique no icone de fechar")
+    public void forRealizadoUmCliqueNoIconeDeFechar() {
+
+    }
+
+
+    @Quando("for realizado um clique no link Create New Account")
+    public void forRealizadoUmCliqueNoLinkCreateNewAccount() {
+
+    }
+
+
+    @Entao("a pagina Create new Account deve ser exibida")
+    public void aPaginaCreateNewAccountDeveSerExibida() {
+
+    }
+
+
+    @Quando("os campos de login forem preenchidos da seguinte forma")
+    public void osCamposDeLoginForemPreenchidosDaSeguinteForma() {
+
+    }
+
+    @Quando("for realizado um clique no botao sign in")
+    public void forRealizadoUmCliqueNoBotaoSignIn() {
+
+    }
+
+    @Entao("deve ser possivel logar no sistema")
+    public void deveSerPossivelLogarNoSistema() {
+
+    }
+
+    @Entao("o sistema deve exibir uma mensagem de erro")
+    public void oSistemaDeveExibirUmaMensagemDeErro() {
+
+    }
+
+    @Entao("o botao sign in deve permanecer desabilitado")
+    public void oBotaoSignInDevePermanecerDesabilitado() {
+
+    }
+}
