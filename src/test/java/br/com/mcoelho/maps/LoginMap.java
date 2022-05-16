424c38dff0518f20bb66a879c2d1b8a28d731b47
Mayra Coelho <mayraccoelho@gmail.com>
2022-05-15 23:04:32 -0300

    inicio map

---
+++ src/test/java/br/com/mcoelho/maps/LoginMap.java
@@ -0,0 +1,16 @@
+package br.com.mcoelho.maps;
+
+import org.openqa.selenium.WebElement;
+
+public class LoginMap {
+    public WebElement btnLogin;
+    public WebElement btnFechar;
+    public WebElement divFechar;
+    public WebElement divFecharModal;
+    public WebElement inpUserName;
+    public WebElement inpPassword;
+    public WebElement btnSignIn;
+    public WebElement inpRemember;
+    public WebElement lnkCreateAccount;
+
+}
---
+++ src/test/java/br/com/mcoelho/pages/LoginPage.java
@@ -0,0 +1,46 @@
+package br.com.mcoelho.pages;
+
+import br.com.mcoelho.core.Driver;
+import br.com.mcoelho.maps.LoginMap;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+    LoginMap loginmap;
+
+    public LoginPage(){
+        loginmap = new LoginMap();
+        PageFactory.initElements(Driver.getDriver(), loginmap);
+    }
+
+    public void clickBtnLogin(){
+        loginmap.btnLogin.click();
+    }
+
+    public void clickDivFecharModal(){
+        loginmap.divFecharModal.click();
+    }
+
+    public void setInpUserName(String username){
+        loginmap.inpUserName.sendKeys(username);
+    }
+
+    public void setInpPassword(String password){
+        loginmap.inpPassword.sendKeys(password);
+    }
+
+    public void clickInpRemember(){
+        loginmap.inpRemember.click();
+    }
+
+    public void clickLnkCreateAccount(){
+        loginmap.lnkCreateAccount.click();
+    }
+
+    public void clickBtnSignIn(){
+        loginmap.btnSignIn.click();
+    }
+
+    public boolean isBtnSignIn(){
+        return loginmap.btnSignIn.isEnabled();
+    }
+}
